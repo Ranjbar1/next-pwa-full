65caae5871acb0698fa46d98c7d8ba6401254877
Abolfazl-HOME <98ranjbar@gmail.com>
2023-12-20 08:57:08 +0330

    without init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,120 @@
 import AudioReactRecorder, {
   RecordState,
 } from "@/components/AudioReactRecorder2";
-import React from "react";
+import React, { useState } from "react";
 
-class App extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      recordState: null,
-      audioData: null,
-    };
-  }
-
-  start = () => {
-    this.setState({
-      recordState: RecordState.START,
-    });
+const App = (props: any) => {
+  const [state, setState] = useState<any>({
+    recordState: null,
+    audioData: null,
+  });
+  const start = () => {
+    setState({ recordState: RecordState.START });
   };
-
-  pause = () => {
-    this.setState({
+  const pause = () => {
+    setState({
       recordState: RecordState.PAUSE,
     });
   };
-
-  stop = () => {
-    this.setState({
+  const stop = () => {
+    setState({
       recordState: RecordState.STOP,
     });
   };
 
-  onStop = (data: any) => {
-    this.setState({
+  const onStop = (data: any) => {
+    setState({
       audioData: data,
     });
     console.log("onStop: audio data", data);
   };
-
-  render() {
-    const { recordState } = this.state;
-
-    return (
-      <div>
-        <AudioReactRecorder
-          state={recordState}
-          onStop={this.onStop}
-          backgroundColor="rgb(255,255,255)"
-        />
-        <audio
-          id="audio"
-          controls
-          src={this.state.audioData ? this.state.audioData.url : null}
-        ></audio>
-        <button id="record" onClick={this.start}>
-          Start
-        </button>
-        <button id="pause" onClick={this.pause}>
-          Pause
-        </button>
-        <button id="stop" onClick={this.stop}>
-          Stop
-        </button>
-      </div>
-    );
-  }
-}
-
+  return (
+    <div>
+      <AudioReactRecorder
+        state={state.recordState}
+        onStop={onStop}
+        backgroundColor="rgb(255,255,255)"
+      />
+      <audio
+        id="audio"
+        controls
+        src={state.audioData ? state.audioData.url : null}
+      ></audio>
+      <button id="record" onClick={start}>
+        Start
+      </button>
+      <button id="pause" onClick={pause}>
+        Pause
+      </button>
+      <button id="stop" onClick={stop}>
+        Stop
+      </button>
+    </div>
+  );
+};
 export default App;
+// class App extends React.Component<any, any> {
+//   constructor(props: any) {
+//     super(props);
+
+//     this.state = {
+//       recordState: null,
+//       audioData: null,
+//     };
+//   }
+
+//   start = () => {
+//     this.setState({
+//       recordState: RecordState.START,
+//     });
+//   };
+
+//   pause = () => {
+//     this.setState({
+//       recordState: RecordState.PAUSE,
+//     });
+//   };
+
+//   stop = () => {
+//     this.setState({
+//       recordState: RecordState.STOP,
+//     });
+//   };
+
+//   onStop = (data: any) => {
+//     this.setState({
+//       audioData: data,
+//     });
+//     console.log("onStop: audio data", data);
+//   };
+
+//   render() {
+//     const { recordState } = this.state;
+
+//     return (
+//       <div>
+//         <AudioReactRecorder
+//           state={recordState}
+//           onStop={this.onStop}
+//           backgroundColor="rgb(255,255,255)"
+//         />
+//         <audio
+//           id="audio"
+//           controls
+//           src={this.state.audioData ? this.state.audioData.url : null}
+//         ></audio>
+//         <button id="record" onClick={this.start}>
+//           Start
+//         </button>
+//         <button id="pause" onClick={this.pause}>
+//           Pause
+//         </button>
+//         <button id="stop" onClick={this.stop}>
+//           Stop
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
