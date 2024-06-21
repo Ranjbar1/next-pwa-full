9188ccacd157478aa2c284a23f65bcfe9779d7ae
abolfazl <98ranjbar@gmail.com>
2024-06-21 23:17:40 +0330

    feature : import assets and manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,40 @@ export default function Document(props: DocumentHeadTagsProps) {
     <Html lang="fa" dir="rtl">
       <Head>
         <DocumentHeadTags {...props} />
+        <link rel="manifest" href="/manifest.json" />
+        <meta charSet="UTF-8" key="charset" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          key="viewport"
+        />
+        <meta name="theme-color" content="#000000" />
+        <link
+          rel="apple-touch-icon"
+          href={`/app-icon/icon-512x512.png`}
+          key="apple"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`/app-icon/icon-72x72.png`}
+          key="icon32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`/app-icon/icon-72x72.png`}
+          key="icon16"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href={`/favicon.png`}
+          key="favicon"
+        />
       </Head>
       <body>
         <Main />
