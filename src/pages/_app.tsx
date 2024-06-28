998630e526c8d2bae28c2cbf19346c9166fe8a89
abolfazl <98ranjbar@gmail.com>
2024-06-28 12:48:08 +0330

    fix : move _document Head & meta to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import '@/styles/globals.css';
 import { components, palette, typography } from '@/theme';
-import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
+import {
+  AppCacheProvider,
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+} from '@mui/material-nextjs/v13-pagesRouter';
 import { Theme, ThemeProvider, createTheme } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
 import createCache from '@emotion/cache';
 import { prefixer } from 'stylis';
 import rtlPlugin from 'stylis-plugin-rtl';
 import { CssBaseline } from '@mui/material';
+import Head from 'next/head';
 declare module '@mui/material/styles' {
   interface Theme {}
   interface ThemeOptions {}
@@ -23,13 +28,55 @@ const theme: Theme = createTheme({
   palette,
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & DocumentHeadTagsProps) {
   return (
-    <AppCacheProvider value={cacheRtl} {...pageProps}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </AppCacheProvider>
+    <>
+      <Head>
+        <DocumentHeadTags {...pageProps.documentHeadTags} />
+        <link rel="manifest" href="/manifest.json" />
+        <meta charSet="UTF-8" key="charset" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          key="viewport"
+        />
+        {/* <meta name="theme-color" content="#000000" /> */}
+        <link
+          rel="apple-touch-icon"
+          href={`/app-icon/icon-512x512.png`}
+          key="apple"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`/app-icon/icon-72x72.png`}
+          key="icon32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`/app-icon/icon-72x72.png`}
+          key="icon16"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href={`/favicon.png`}
+          key="favicon"
+        />
+      </Head>{' '}
+      <AppCacheProvider value={cacheRtl} {...pageProps}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </AppCacheProvider>
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,50 +1,10 @@
+import { documentGetInitialProps } from '@mui/material-nextjs/v13-pagesRouter';
 import { Html, Head, Main, NextScript } from 'next/document';
-import {
-  DocumentHeadTags,
-  documentGetInitialProps,
-} from '@mui/material-nextjs/v13-pagesRouter';
-import type { DocumentHeadTagsProps } from '@mui/material-nextjs/v13-pagesRouter';
 
-export default function Document(props: DocumentHeadTagsProps) {
+export default function Document() {
   return (
     <Html lang="fa" dir="rtl">
-      <Head>
-        <DocumentHeadTags {...props} />
-        <link rel="manifest" href="/manifest.json" />
-        <meta charSet="UTF-8" key="charset" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-          key="viewport"
-        />
-        <meta name="theme-color" content="#000000" />
-        <link
-          rel="apple-touch-icon"
-          href={`/app-icon/icon-512x512.png`}
-          key="apple"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={`/app-icon/icon-72x72.png`}
-          key="icon32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href={`/app-icon/icon-72x72.png`}
-          key="icon16"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href={`/favicon.png`}
-          key="favicon"
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
